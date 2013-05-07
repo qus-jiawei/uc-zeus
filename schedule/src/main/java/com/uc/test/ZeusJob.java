401ca00aa7cb84b1ed323f85412352cddc2c18cf
qus-jiawei <qus.jiawei@gmail.com>
2013-05-07 20:38:55 +0800

    fix bug

--- schedule/src/main/java/com/uc/test/ZeusJob.java
+++ schedule/src/main/java/com/uc/test/ZeusJob.java
@@ -1,28 +1,24 @@
 package com.uc.test;
 
 import org.apache.hadoop.conf.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.taobao.zeus.schedule.mvc.ScheduleInfoLog;
 
 public class ZeusJob {
-	private static Logger log=LoggerFactory.getLogger(ScheduleInfoLog.class);
 	public static void main(String[] args){
 		
 		try {
 			Thread.sleep(1000);
-			log.info("sleep");
+			System.out.println("sleep");
+//			System.out.println("sleep");
 			Thread.sleep(1000);
 			Configuration conf=new Configuration(false);
 			String temp = conf.get("a");
-			log.info("a is:"+temp);
+			System.out.println("a is:"+temp);
 			int time = conf.getInt("sleep",0);
-			log.info("time is :"+time);
+			System.out.println("time is :"+time);
 			Thread.sleep(time*1000);
 			
 		} catch (InterruptedException e) {
-			log.info("catch exception",e);
+			System.out.println("catch exception");
 		}
 		
 	}

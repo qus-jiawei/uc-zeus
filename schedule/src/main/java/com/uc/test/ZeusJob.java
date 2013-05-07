1216a82bc3ed0ea0e3483337670d3dbd3091cee4
qus-jiawei <qus.jiawei@gmail.com>
2013-05-07 20:53:04 +0800

    change print to write

--- schedule/src/main/java/com/uc/test/ZeusJob.java
+++ schedule/src/main/java/com/uc/test/ZeusJob.java
@@ -5,11 +5,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 
 public class ZeusJob {
 	public static void main(String[] args){
 		
 		try {
+			String workDir=System.getenv("instance.workDir");
+			Configuration conf=new Configuration(false);
+			conf.addResource(new Path(workDir+File.separator+"hadoop-site.xml"));
+			
+			
 			FileWriter fw = new FileWriter(new File("/home/qiujw/zeus/logs/zeusjob.log"));
 			Thread.sleep(1000);
 			fw.write("sleep");
@@ -17,7 +23,7 @@ public class ZeusJob {
 //			System.out.println("sleep");
 			Thread.sleep(1000);
 			fw.write("sleep");
-			Configuration conf=new Configuration(false);
+//			Configuration conf=new Configuration(false);
 			String temp = conf.get("a");
 			System.out.println("a is:"+temp);
 			fw.write("a is:"+temp);
